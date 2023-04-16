06a00dd76173d2439ed7d7a7751436cd8949bd72
lanzuln <71728788+lanzuln@users.noreply.github.com>
2023-04-16 23:29:34 +0600

    assignment done

---
+++ m11-assignment.js
@@ -0,0 +1,12 @@
+function multiplicationTable(num) {
+    for (let i = 1; i <= 10; i++) {
+        const product = num * i;
+        console.log(num + " x " + i + " = " + product);
+    }
+}
+
+
+// here the different input 
+multiplicationTable(2);
+multiplicationTable(3);
+multiplicationTable(4);
